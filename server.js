ecd275a84ca769511ef1d8325fa85ea68bd69638
amirkazakov333 <amirkazakov333@gmail.com>
2025-07-26 13:02:51 +0300

    some changes in server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/", router);
 
-app.listen(port, () => console.log(`Server running on server ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
